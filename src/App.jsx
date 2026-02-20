df9aab55ff65def5bb5cf64005370596cbc41af2
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2026-02-20 13:32:00 -0500

    feat: integrate Tailwind CSS and add TailwindText component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 // import { CardWithStyles } from "./components/Card/CardWithStyles"
 import { Button } from "./components/Button/Button"
 import "./App.css"
+import { TailwindText } from "./components/Text/Text"
 
 function App() {
 
   return (
     <>
-      <h1>My React App</h1>
-      <Button color="orange">
+      <h1 >My React App</h1>
+      <TailwindText />
+      {/* <Button color="orange">
         <p>Orange Button</p>
       </Button>
       <Button>
@@ -16,7 +18,7 @@ function App() {
       </Button>
       <Button color="yellow">
         <p>Yellow Button</p>
-      </Button>
+      </Button> */}
     </>
   )
 }
---
+++ src/components/Text/Text.jsx
@@ -0,0 +1,9 @@
+const TailwindText = () => {
+  return (
+    <p className="text-3xl font-bold underline text-orange-500">
+      This is a Tailwind CSS styled text component.
+    </p>
+  );
+}
+
+export { TailwindText };
